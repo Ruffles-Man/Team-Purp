8883dccae24c8d5247f64b4fc62468642093e565
NikOhNo <80977626+NikOhNo@users.noreply.github.com>
2026-04-20 11:34:44 -0700

    Basic Movement and Sprinting

--- Tools of the Trade/Assets/Scripts/PlayerMovement.cs
+++ Tools of the Trade/Assets/Scripts/PlayerMovement.cs
@@ -4,11 +4,24 @@ using UnityEngine.InputSystem;
 [RequireComponent(typeof(CharacterController))]
 public class PlayerMovement : MonoBehaviour
 {
+    public float MaxSpeed => moveSpeed * sprintMultiplier;
+    public float CurrentSpeed => moveSpeed * (sprinting ? sprintMultiplier : 1f);
+    float SpeedProgress => SmoothMoveInput.magnitude * CurrentSpeed / MaxSpeed; // Calculate speed as a percentage of max speed
+
+    public Vector2 SmoothMoveInput = Vector2.zero;
+    [SerializeField] private float smoothTime = 0.25f;
+    [SerializeField] private float sprintMultiplier = 1.5f;
     [SerializeField] private float moveSpeed = 5f;
     [SerializeField, HideInInspector] GameObject body;
+
     private CharacterController controller;
     private Animator animator;
     private InputSystem_Actions inputActions;
+    private Vector2 velocity = Vector2.zero;
+    private bool sprinting = false;
+
+    // hashed animation references
+    private readonly int speedHash = Animator.StringToHash("Speed");
 
     private void Awake()
     {
@@ -29,23 +42,36 @@ public class PlayerMovement : MonoBehaviour
 
     void Update()
     {
+        HandleSprint(inputActions);
         HandleMove(inputActions);
+    }
+
+    private void HandleSprint(InputSystem_Actions actions)
+    {
+        sprinting = actions.Player.Sprint.IsPressed();
     }
 
     private void HandleMove(InputSystem_Actions actions)
     {
+        // input processing
         Vector2 moveInput = actions.Player.Move.ReadValue<Vector2>();
-        Vector3 moveDirection = new(moveInput.x, 0, moveInput.y);
+        SmoothMoveInput = Vector2.SmoothDamp(SmoothMoveInput, moveInput, ref velocity, smoothTime); 
+       
+        // movement
+        Vector3 moveDirection = new(SmoothMoveInput.x, 0, SmoothMoveInput.y);
+        moveDirection = transform.TransformDirection(moveDirection); // Convert local movement to world space
+        Vector3 moveVector = CurrentSpeed * Time.deltaTime * moveDirection;
+        controller.Move(moveVector);
+
+        // animation
         if (moveDirection != Vector3.zero)
         {
-            animator.SetFloat("Speed", moveDirection.magnitude); // Update the animator with the movement speed
+            animator.SetFloat(speedHash, SpeedProgress); // Update the animator with the movement speed
             body.transform.rotation = Quaternion.LookRotation(moveDirection.normalized); // Rotate the body to face the movement direction
         }
         else
         {
-            animator.SetFloat("Speed", 0); // Set speed to 0 when not moving
+            animator.SetFloat(speedHash, 0); // Set speed to 0 when not moving
         }
-        moveDirection = transform.TransformDirection(moveDirection); // Convert local movement to world space
-        controller.Move(moveSpeed * Time.deltaTime * moveDirection);
     }
 }
