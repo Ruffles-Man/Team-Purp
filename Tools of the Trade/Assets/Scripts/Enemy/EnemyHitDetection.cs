89e99ec3811d9f8e9c1ce23cc00aad26141fc412
Ruffles-Man <simjuillian@gmail.com>
2026-05-31 10:35:06 -0700

    Enemy to Player Hit Detection however the player character now wil strangely hover in the air as if stepping on something if too close

---
+++ Tools of the Trade/Assets/Scripts/Enemy/EnemyHitDetection.cs
@@ -0,0 +1,71 @@
+using System;
+using System.Collections.Generic;
+using UnityEngine;
+[RequireComponent(typeof(CharacterController))]
+[RequireComponent(typeof(PlayerVFX))]
+public class EnemyHitDetection : LockableMonoBehavior
+{
+    public Transform hitboxOrigin;
+    public MovesetData moveset;
+    Animator animator;
+    //PlayerVFX playerVFX;
+    
+    void Awake()
+    {
+        animator = GetComponentInChildren<Animator>();
+        // playerVFX = GetComponent<PlayerVFX>();
+    }
+
+    void Update()
+    {
+        CastHitboxes();
+    }
+
+    private ClipHitboxBinding lastBinding;     
+    private readonly HashSet<IHittable> hitThisAttack = new();
+    public void CastHitboxes()
+    {
+        var binding = AnimatorInfo.GetCurrentBinding(moveset, animator);
+
+        // flush if we've moved to a different attack
+        if (binding != lastBinding)
+        {
+            hitThisAttack.Clear();
+
+            // // Release when leaving an attack, request when entering one
+            // if (lastBinding != null) playerVFX.ReleaseTrails();
+            // if (binding != null) playerVFX.RequestTrails();
+
+            lastBinding = binding;
+        }
+
+        if (binding == null) return;
+
+        int currentFrame = AnimatorInfo.GetCurrentFrameFromAnimator(animator);
+        
+        foreach (var hitbox in binding.hitboxes)
+        {
+            if (hitbox.HitboxActive(currentFrame))
+            {
+                ProcessHitbox(hitbox);
+            }
+        }
+    }
+
+    private void ProcessHitbox(AttackData hitbox)
+    {
+        var (colliders, worldOrigin) = hitbox.CastCheck(hitboxOrigin);
+        foreach (var collider in colliders)
+        {
+            if (collider.TryGetComponent(out IHittable hittable) && hitThisAttack.Add(hittable))
+            {
+                hittable.TakeHit(new HitInfo
+                {
+                    attackType = hitbox.attackType,
+                    damage = hitbox.damage,
+                    position = collider.ClosestPoint(worldOrigin)  // closest point on target to hitbox center
+                });
+            }
+        }
+    }
+}
