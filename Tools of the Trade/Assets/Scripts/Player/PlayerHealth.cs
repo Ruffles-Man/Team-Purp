ec18349c7c1417cff1bc7b16067666928d7fe83f
Ruffles-Man <simjuillian@gmail.com>
2026-06-01 12:06:36 -0700

    Fixed Player Prefabs not taking damage

--- Tools of the Trade/Assets/Scripts/Player/PlayerHealth.cs
+++ Tools of the Trade/Assets/Scripts/Player/PlayerHealth.cs
@@ -1,12 +1,38 @@
 using System;
 using UnityEngine;
 using UnityEngine.Events;
+using System.Linq;
 
 public class PlayerHealth : HealthBase
 {
-    void Awake()
+    [SerializeField] private HealthBar healthBar;
+
+    void OnEnable()
     {
-        // TODO: want to load this from a file so health is consistent across scenes
+        var healthBar = UnityEngine.Object.FindObjectsByType<HealthBar>().Where(hpBar => hpBar.CompareTag("Player")).Single();
+        onHealthChanged.AddListener(healthBar.UpdateValue);
+    }
+
+    void OnDisable()
+    {
+        onHealthChanged.RemoveListener(healthBar.UpdateValue);
+    }
+
+    private void Awake()
+    {
         currentHP = maxHP;
+        // If the inspector slot is empty (which it will be on a Prefab), find it dynamically!
+        if (healthBar == null)
+        {
+            GameObject managerObj = GameObject.Find("PlayerHealthBar");
+            if (managerObj != null)
+            {
+                healthBar = managerObj.GetComponent<HealthBar>();
+            }
+            else
+            {
+                Debug.LogWarning($"[PlayerHealth] Could not find a PlayerHealthBar in the scene context of {gameObject.name}!");
+            }
+        }
     }
 }
